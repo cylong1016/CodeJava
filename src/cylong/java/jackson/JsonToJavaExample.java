50a6397f6a5a3a9e2f9c89af0d3363fa7afd4a21
cylong1016 <cyl19941016@gmail.com>
2017-03-31 09:18:15 +0800

    Jackson 示例代码

---
+++ src/cylong/java/jackson/JsonToJavaExample.java
@@ -0,0 +1,49 @@
+package cylong.java.jackson;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Read JSON string and convert it back to a Java object.
+ * @see <a href=https://www.mkyong.com/java/jackson-2-convert-java-object-to-from-json>
+ *      Jackson 2 Convert Java Object to / from JSON</a>
+ * @author cylong
+ * @version 2017年3月31日 上午9:05:18
+ */
+public class JsonToJavaExample {
+
+	public static void main(String[] args) {
+		JsonToJavaExample.run();
+	}
+
+	private static void run() {
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+
+			// Convert JSON string from file to Object
+			Staff staff = mapper.readValue(new File("data/staff.json"), Staff.class);
+			System.out.println(staff);
+
+			// Convert JSON string to Object
+			String jsonInString = "{\"name\":\"cylong\",\"salary\":7500,\"skills\":[\"java\",\"python\"]}";
+			Staff staff1 = mapper.readValue(jsonInString, Staff.class);
+			System.out.println(staff1);
+
+			//Pretty print
+			String prettyStaff1 = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(staff1);
+			System.out.println(prettyStaff1);
+
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/cylong/java/jackson/Staff.java
@@ -0,0 +1,79 @@
+package cylong.java.jackson;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @see <a href=https://www.mkyong.com/java/jackson-2-convert-java-object-to-from-json>
+ *      Jackson 2 Convert Java Object to / from JSON</a>
+ * @author cylong
+ * @version 2017年3月31日 上午8:50:04
+ */
+public class Staff {
+
+	private String name;
+	private int age;
+	private String position;
+	private BigDecimal salary;
+	private List<String> skills;
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPosition() {
+		return this.position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public BigDecimal getSalary() {
+		return this.salary;
+	}
+
+	public void setSalary(BigDecimal salary) {
+		this.salary = salary;
+	}
+
+	public List<String> getSkills() {
+		return this.skills;
+	}
+
+	public void setSkills(List<String> skills) {
+		this.skills = skills;
+	}
+
+	public static Staff createDummyObject() {
+
+		Staff staff = new Staff();
+
+		staff.setName("cylong");
+		staff.setAge(33);
+		staff.setPosition("Developer");
+		staff.setSalary(new BigDecimal("7500"));
+
+		List<String> skills = new ArrayList<>();
+		skills.add("java");
+		skills.add("python");
+
+		staff.setSkills(skills);
+
+		return staff;
+	}
+
+}
